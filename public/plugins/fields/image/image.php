209ee6499702a77c738b4f38b91f0bdb42babe08
AndyGaskell <ag@ssofb.co.uk>
2022-01-12 11:19:51 +0000

    upload and save working

--- public/plugins/fields/image/image.php
+++ public/plugins/fields/image/image.php
@@ -48,12 +48,12 @@ class PlgFieldsImage extends \Joomla\Component\Fields\Administrator\Plugin\Field
         #echo "<pre>form: " . print_r($form, TRUE) . "</pre>";
         
         $this->ilog("onCustomFieldsPrepareDom");
-        $this->ilog("field: " . print_r($field, TRUE));
-        $this->ilog("parent: " . print_r($parent, TRUE));
-        $this->ilog("form: " . print_r($form, TRUE));        
+        #$this->ilog("field: " . print_r($field, TRUE));
+        #$this->ilog("parent: " . print_r($parent, TRUE));
+        #$this->ilog("form: " . print_r($form, TRUE));        
 
         $fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
-        $this->ilog("fieldNode: " . print_r($fieldNode, TRUE));
+        #$this->ilog("fieldNode: " . print_r($fieldNode, TRUE));
 
 		if (!$fieldNode ){
 			
@@ -180,22 +180,80 @@ class PlgFieldsImage extends \Joomla\Component\Fields\Administrator\Plugin\Field
     public function onContentAfterSave($context, &$article, $isNew) {
         $this->ilog("onContentAfterSave");
         $this->ilog("article id: " . $article->id);
-        $this->ilog("jcFields: " . print_r($article->jcFields, TRUE) );
 
 
+        $this->ilog("POST: " . print_r($_POST, true));
+        $this->ilog("GET: " . print_r($_GET, true));
+        $this->ilog("FILES: " . print_r($_FILES, true));
+        $headers_array = array_change_key_case(getallheaders(), CASE_LOWER);
+        $this->ilog("Headers: " . print_r($headers_array, true), 6);
 
-        JLoader::register('FieldsHelper', JPATH_ADMINISTRATOR . '/components/com_fields/helpers/fields.php');
-        $custom_fields = FieldsHelper::getFields($context, $article);
-        $custom_fields_by_name = \Joomla\Utilities\ArrayHelper::pivot($custom_fields, 'name');
-        $this->ilog("custom_fields: " . print_r($custom_fields, TRUE));
-        $this->ilog("custom_fields_by_name: " . print_r($custom_fields_by_name, TRUE));
+
+        $input = Factory::getApplication()->input;
+        $files = $input->files->get('jform');
+        $this->ilog("files: " . print_r($files, true), 6);
 
 
+        
         JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_fields/models');
         $model_field = JModelLegacy::getInstance('Field', 'FieldsModel', ['ignore_request' => true]);
+        #$this->ilog("model_field: " . print_r($model_field, true));
 
-        //set the value using field model instead to make change permanent in db
-        $model_field->setFieldValue(1, $article->id, "3D");
+        jimport('joomla.filesystem.file');
+
+        foreach ( $files['com_fields'] AS $field_name=>$field_data ) {
+
+            if ( $field_data['error'] == 0 ) {
+                $this->ilog("field_name: " . $field_name);
+                $this->ilog("field_data: " . print_r($field_data, true));
+    
+                # set image filename
+                list($file_type, $file_extension) = explode("/", $field_data['type']);
+                $file_url = "images/image-field-file_id" . $article->id . "_"  . date("Y-m-d_H-i-s") . "_" . rand(1000, 9999) . "." . $file_extension;
+                $file_name = JPATH_SITE . "/" . $file_url;
+                $this->ilog("file_url: " . $file_url);
+                $this->ilog("file_name: " . $file_name);
+                
+
+
+                # move image into dir
+                File::upload($field_data['tmp_name'], $file_name);
+                
+                
+                # set filename as field value
+                #$jform = $input->get('jform', array(), 'array');
+                #$jform['com_fields'][$field_name] = $filename;
+                #$jform['articletext'] = date("Y-m-d_H-i-s");
+                #$input->set("jform", $jform);
+    
+                #$this->ilog("input: " . print_r($input, true));
+    
+                #$data['com_fields'][$field_name] = $filename;
+                #$data['articletext'] = date("Y-m-d_H-i-s");
+    
+                $db = JFactory::getDbo();
+                $query = $db
+                    ->getQuery(true)
+                    ->select('id')
+                    ->from($db->quoteName('#__fields'))
+                    ->where($db->quoteName('name') . " = " . $db->quote($field_name));
+                $db->setQuery($query);
+                $field_id = $db->loadResult();
+                $this->ilog("field_id: " . $field_id);
+    
+    
+                //set the value using field model instead to make change permanent in db
+                $model_field->setFieldValue($field_id, $article->id, $file_url);
+            }
+
+
+
+        }
+
+
+
+
+
 
     
     }
