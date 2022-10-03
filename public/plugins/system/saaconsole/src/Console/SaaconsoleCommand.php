4962e88583698cd9c97bdcb6202946000096f409
Andy Gaskell <ag@ssofb.co.uk>
2022-10-03 21:59:01 +0100

    refactoring and initial version of geo box tagging
    Re Auto tag places in a lat/lon area, eg, sunnybank, mounhooley, the green/merchant quarter, beach tunnel, TX, bridge of don farm, westburn park, clifton road shopping center, denburn, former hydrosun, fittie, greyhope, garthdee

--- public/plugins/system/saaconsole/src/Console/SaaconsoleCommand.php
+++ public/plugins/system/saaconsole/src/Console/SaaconsoleCommand.php
@@ -29,6 +29,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Joomla\CMS\Date\Date;
 use JLoader;
 #use Saa_helper\Saa_helper;
 use Joomla\CMS\Saa_helper\Saa_helper;
@@ -133,6 +134,45 @@ class SaaconsoleCommand extends AbstractCommand
 
 		$symfonyStyle->text('action: ' . $action);
 
+
+		if ($action == "geo") {
+			self::do_geo($symfonyStyle);
+
+		} elseif ( $action == "geozones" ) {
+			self::do_geozones($symfonyStyle);
+
+		} elseif ( $action == "images") {
+			self::do_images($symfonyStyle, );
+
+		} elseif ( $action == "images_clear_out") {
+			self::do_images($symfonyStyle, $action);
+
+		} elseif ( $action == "social") {
+			self::do_social($symfonyStyle);
+
+		} else {
+			$symfonyStyle->text('No recognised action');
+
+		}
+
+		$symfonyStyle->success('StreetArtAberdeen CLI');
+
+		return 0;
+	}
+
+
+	/**
+	* do_images
+	* 
+	* Sets titles based on Geo lookup
+	* php /var/www/html/streetartaberdeen/cli/joomla.php  saaconsole:action images
+	*
+	* @param 	symfonyStyle 	A symfonyStyle object for IO#
+	* @param	string			A sting with options from the action value
+	*
+	* @return 	void			no return value
+	*/
+	public function do_images($symfonyStyle, $action=""){
 		# get all the image filenames
 		$db = Factory::getDbo();
 		$query = $db
@@ -154,7 +194,7 @@ class SaaconsoleCommand extends AbstractCommand
 			$symfonyStyle->text('image: ' . $image);
 			# run the helper functions against them
 
-			if ( $action == "clear_out") {
+			if ( $action == "images_clear_out") {
 				$clear_out_image_out = Saa_helper::clear_out_image($image);
 				$symfonyStyle->text('clear_out_image_out: ' . $clear_out_image_out);
 			}
@@ -162,9 +202,22 @@ class SaaconsoleCommand extends AbstractCommand
 			$check_image_out = Saa_helper::check_image($image);
 			$symfonyStyle->text('check_image_out: ' . $check_image_out);
 		}
+		return;		
+	}
 
+
+	/**
+	* do_geo
+	* 
+	* Sets titles based on Geo lookup
+	* php /var/www/html/streetartaberdeen/cli/joomla.php  saaconsole:action geo
+	*
+	* @param 	symfonyStyle 	A symfonyStyle object for IO
+	*
+	* @return 	void			no return value
+	*/
+	public function do_geo($symfonyStyle){
 		# reverse geo lookup
-		# TODO: get values from params
 		#$map_api_key = $this->params->get('map_api_key');
 		#$map_default_lat_lon = $this->params->get('map_default_lat_lon'); 
 		$map_api_key = "AIzaSyDmXMhPB4QnspmKY49FP3YnlhRp7_ao1CA";
@@ -221,8 +274,133 @@ class SaaconsoleCommand extends AbstractCommand
 			}
 		}
 
-		$symfonyStyle->success('StreetArtAberdeen CLI');
+		return;
+	}
 
-		return 0;
+
+	/**
+	* do_geozones
+	* 
+	* Tags items based on lat/lon
+	* php /var/www/html/streetartaberdeen/cli/joomla.php  saaconsole:action geozones
+	*
+	* @param 	symfonyStyle 	A symfonyStyle object for IO
+	*
+	* @return 	void			no return value
+	*/
+	public function do_geozones($symfonyStyle){
+
+		# get all art
+		$db = Factory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select( array('value', 'id', 'title', 'alias', 'ucm_id') );
+		$query->from($db->quoteName('#__fields_values'));
+		$query->from($db->quoteName('#__content'));
+		$query->from($db->quoteName('#__ucm_base'));
+		$query->where($db->quoteName('field_id') . " = 2");
+		$query->where($db->quoteName('item_id') . " = " . $db->quoteName('id'));
+		$query->where($db->quoteName('id') . " = " . $db->quoteName('ucm_item_id'));
+		$query->where($db->quoteName('state') . " = 1");
+		$db->setQuery($query);
+		$articles = $db->loadAssocList();
+
+		# geo zone boxes
+		$boxes = Array();
+
+		# sunnybank 
+		$boxes[] = Array(
+			"alias" => "sunnybank",
+			"id" => 38,
+			"nw-lat" => 57.162238402474514,
+			"nw-lon" => -2.105860783020903,
+			"se-lat" => 57.1607824633046,
+			"se-lon" => -2.1018278970248505
+		);
+
+
+		# loop through artworks
+		foreach( $articles AS $article ) {
+			#$symfonyStyle->text(' ');
+			$symfonyStyle->text("\n\n Art: " . $article['title'] . " (id: " . $article['id'] . ", alias: " . $article['alias'] . "), lat/lon: " . $article['value'] );
+			list($art_lat, $art_lon) = explode(",", $article['value']);
+
+			# loop through boxes
+			foreach ( $boxes AS $box ) {
+				$symfonyStyle->text("Box: \n" . print_r($box, TRUE) );
+
+				# see if it is in this box
+				if ( $art_lat < $box["nw-lat"] AND $art_lat > $box["se-lat"] AND $art_lon > $box["nw-lon"] AND $art_lon < $box["se-lon"] ) {
+					$symfonyStyle->text("Item " . $article['id'] . " is in the " . $box["alias"] . " box");
+
+					# check them item doesn't have this tag already
+					$query = $db->getQuery(true);
+					$query->select(array('COUNT(*)'));
+					$query->where($db->quoteName('type_alias') . " = " . $db->quote('com_content.article'));
+					$query->where($db->quoteName('content_item_id') . " = " . $db->quote($article['id']));
+					$query->where($db->quoteName('tag_id') . " = " . $db->quote($box["id"]));
+					$query->from($db->quoteName('#__contentitem_tag_map'));
+					$db->setQuery($query);
+					$count = $db->loadResult();
+					$symfonyStyle->text("matching tag count: " . $count . " from " . $query);
+
+					$dt_obj = new Date('now');
+					$sql_datetime = $dt_obj->toSQL();
+
+					# Test 
+					# https://streetartaberdeen.org/gallery/354
+					# Article: Near 1 Sunnyside Terrace, Aberdeen AB24 3NB, UK (id: 1193, alias: 354), lat/lon: 57.161112437766306,-2.1045888443866256
+
+					if ($count == 0) {
+						# insert a tag
+						$symfonyStyle->text("Not got this tag, so add it");
+						# like INSERT INTO `s3ib7_contentitem_tag_map` (`type_alias`, `core_content_id`, `content_item_id`, `tag_id`, `tag_date`, `type_id`) VALUES('com_content.article', 699, 1398, 39, '2022-10-03 10:44:58', 1);
+						$query = $db->getQuery(true);
+						$columns = array('type_alias', 'core_content_id', 'content_item_id', 'tag_id', 'tag_date', 'type_id');
+						$values = array($db->quote('com_content.article'), $db->quote($article['ucm_id']), $db->quote($article['id']), $db->quote($box['id']), $db->quote($sql_datetime), 1); 
+						$query->insert($db->quoteName('#__contentitem_tag_map'));
+						$query->columns($db->quoteName($columns));
+						$query->values(implode(',', $values));
+						$db->setQuery($query);
+						$symfonyStyle->text("Insert query " . $query);
+						$result = $db->execute();
+						$symfonyStyle->text("Result: " . $result);
+					}
+				} else {
+					$symfonyStyle->text("Not in the box");
+				}
+			}
+		}
+		return;
+	}
+
+
+
+	/**
+	* do_social
+	* 
+	* Posts an item to social media
+	*
+	* @param 	symfonyStyle 	A symfonyStyle object for IO
+	*
+	* @return 	void			no return value
+	*/
+	public function do_social($symfonyStyle){
+
+		$symfonyStyle->text('Running do_social');
+
+		/*
+		https://developers.facebook.com/blog/post/2021/01/26/introducing-instagram-content-publishing-api/
+
+		https://developers.facebook.com/docs/instagram-api/guides/content-publishing/#access-tokens
+		https://developers.facebook.com/docs/instagram-api/guides/content-publishing/#single-media-posts
+
+		https://developers.facebook.com/docs/instagram-api#instagram-graph-api
+
+		https://developers.facebook.com/docs/graph-api/overview
+
+		*/
+
+
+		return;
 	}
 }
