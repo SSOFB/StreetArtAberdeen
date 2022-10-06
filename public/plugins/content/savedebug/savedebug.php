314c0b58d2fcb6cdb151206f8e2ee6b3a45a4cdc
AndyGaskell <ag@ssofb.co.uk>
2022-10-06 10:09:51 +0100

    tidy up of initial version

--- public/plugins/content/savedebug/savedebug.php
+++ public/plugins/content/savedebug/savedebug.php
@@ -9,9 +9,7 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\String\StringHelper;
 
 /**
  * Email cloak plugin class.
@@ -20,60 +18,60 @@ use Joomla\String\StringHelper;
  */
 class PlgContentSavedebug extends CMSPlugin
 {
-	/**
-	 * The Application object
-	 *
-	 * @var    JApplicationSite
-	 * @since  3.9.0
-	 */
-	protected $app;
+    /**
+     * The Application object
+     *
+     * @var    JApplicationSite
+     * @since  3.9.0
+     */
+    protected $app;
 
 
-	/**
-	 * Content is passed by reference. Method is called before the content is saved.
+    /**
+     * Content is passed by reference. Method is called before the content is saved.
      * https://docs.joomla.org/Plugin/Events/Content#onContentBeforeSave
-	 *
-	 * @param   string  $context  The context of the content passed to the plugin (added in 1.6).
-	 * @param   object  $article  A JTableContent object.
-	 * @param   bool    $isNew    If the content is just about to be created.
+     *
+     * @param   string  $context  The context of the content passed to the plugin (added in 1.6).
+     * @param   object  $article  A JTableContent object.
+     * @param   bool    $isNew    If the content is just about to be created.
      * @param   array   data    The data to save. 
-	 *
-	 * @return  void
-	 *
-	 * @since   2.5
-	 */
-	public function onContentBeforeSave($context, &$article, $isNew, &$data=Array()) {
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
+    public function onContentBeforeSave($context, &$article, $isNew, &$data=Array()) {
         $this->ilog("onContentBeforeSave");
-		$this->ilog("article id: " . $article->id);
-		$this->ilog("context: " . $context);
-		$this->ilog("POST: " . print_r($_POST, true));
-		$this->ilog("GET: " . print_r($_GET, true));
-		$this->ilog("FILES: " . print_r($_FILES, true));
-		$this->ilog("Headers: " . print_r(getallheaders(), true));
+        $this->ilog("article id: " . $article->id);
+        $this->ilog("context: " . $context);
+        $this->ilog("POST: " . print_r($_POST, true));
+        $this->ilog("GET: " . print_r($_GET, true));
+        $this->ilog("FILES: " . print_r($_FILES, true));
+        $this->ilog("Headers: " . print_r(getallheaders(), true));
     }
 
 
-	/**
-	 * Content is passed by reference. Method is called after the content is saved.
+    /**
+     * Content is passed by reference. Method is called after the content is saved.
      * https://docs.joomla.org/Plugin/Events/Content#onContentAfterSave
-	 *
-	 * @param   string  $context  The context of the content passed to the plugin (added in 1.6).
-	 * @param   object  $article  A JTableContent object.
-	 * @param   bool    $isNew    If the content is just about to be created.
-	 *
-	 * @return  void
-	 *
-	 * @since   2.5
-	 */
+     *
+     * @param   string  $context  The context of the content passed to the plugin (added in 1.6).
+     * @param   object  $article  A JTableContent object.
+     * @param   bool    $isNew    If the content is just about to be created.
+     *
+     * @return  void
+     *
+     * @since   2.5
+     */
     public function onContentAfterSave($context, &$article, $isNew) {
-		$this->ilog("onContentAfterSave");
-		$this->ilog("article id: " . $article->id);
-		$this->ilog("context: " . $context);
-		$this->ilog("POST: " . print_r($_POST, true));
-		$this->ilog("GET: " . print_r($_GET, true));
-		$this->ilog("FILES: " . print_r($_FILES, true));
-		$this->ilog("Headers: " . print_r(getallheaders(), true));
-	}
+        $this->ilog("onContentAfterSave");
+        $this->ilog("article id: " . $article->id);
+        $this->ilog("context: " . $context);
+        $this->ilog("POST: " . print_r($_POST, true));
+        $this->ilog("GET: " . print_r($_GET, true));
+        $this->ilog("FILES: " . print_r($_FILES, true));
+        $this->ilog("Headers: " . print_r(getallheaders(), true));
+    }
 
 
     /**
