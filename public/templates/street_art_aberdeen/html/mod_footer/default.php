8b15fb869428900878714eb84526d5a2e876b5fd
AndyGaskell <ag@ssofb.co.uk>
2022-10-06 10:14:18 +0100

    removing the time from the footer

--- public/templates/street_art_aberdeen/html/mod_footer/default.php
+++ public/templates/street_art_aberdeen/html/mod_footer/default.php
@@ -3,16 +3,13 @@
  * @package     Joomla.Site
  * @subpackage  mod_footer
  *
- * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2022 SSOFB
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
-#JLoader::register('saa_helper', 'templates/street_art_aberdeen/html/saa_helper.php'); 
-#echo saa_helper::tester("hello");
-
 ?>
 <div class="mod-footer">
-	<div class="footer1"><?php echo $app->getCfg( 'sitename' ) . " - " . date('l jS \of F Y, g:ia') ?></div>
+	<div class="footer1"><?php echo $app->getCfg( 'sitename' ) . " - " . date('l jS \of F Y') ?></div>
 </div>
