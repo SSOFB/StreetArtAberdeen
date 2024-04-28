d6146aad6ed14ddd0b39c90752b08616271c904a
Andy Gaskell <ag@ssofb.co.uk>
2024-04-28 15:01:15 +0100

    more tests
    See also...
    https://joomla.stackexchange.com/questions/33788/joomla-api-returns-500-error-but-works-when-debug-is-enabled

--- tools/api_test.sh
+++ tools/api_test.sh
@@ -8,3 +8,11 @@ curl -X GET "https://streetartaberdeen.com/api/index.php/v1/content/articles" \
      -H "Content-Type: application/json" \
      -H "Accept: application/vnd.api+json" \
      -H "Authorization: Bearer $TOKEN"  
+
+curl -X GET "https://streetartaberdeen.com/api/index.php/v1/content/articles" -H "Authorization: Bearer $TOKEN"  
+
+
+#curl --location --request GET "http://127.0.0.1:8000/api/index.php/v1/users" --header "X-Joomla-Token: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+
+curl --location --request GET "https://streetartaberdeen.com/api/index.php/v1/content/articles" --header "X-Joomla-Token: $TOKEN"
